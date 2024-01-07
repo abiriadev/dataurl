4fb1449dafca9249875b17521d43b450c69dc432
Abiria <abiria.dev@gmail.com>
2024-01-08 00:25:25 +0900

    feat: use urfave/cli

--- main.go
+++ main.go
@@ -1,34 +1,55 @@
 package main
 
 import (
-	"flag"
 	"mime"
 	"os"
 	"path/filepath"
 
 	"github.com/abiriadev/dataurl/dataurl"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	flag.Parse()
+	if err := (&cli.App{
+		Name:  "dataurl",
+		Usage: "print dataurl of given data",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "mime",
+				Usage: "Set MIME type for data",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			// mime := ctx.String("mime")
 
-	file := flag.Arg(0)
-	if file == "" {
-		panic("provide at least one file")
-	}
+			file := ctx.Args().Get(0)
 
-	h, err := os.Open(file)
-	if err != nil {
+			if file == "" {
+				panic("provide at least one file")
+			}
+
+			h, err := os.Open(file)
+
+			if err != nil {
+				panic(err)
+			}
+
+			mime := mime.TypeByExtension(filepath.Ext(file))
+
+			if mime == "" {
+				panic("unknown file type")
+			}
+
+			err = dataurl.ToDataUrl(dataurl.Mime(mime), h, os.Stdout)
+
+			if err != nil {
+				panic(err)
+			}
+
+			return nil
+		},
+	}).Run(os.Args); err != nil {
 		panic(err)
 	}
 
-	mime := mime.TypeByExtension(filepath.Ext(file))
-	if mime == "" {
-		panic("unknown file type")
-	}
-
-	err = dataurl.ToDataUrl(mime, h, os.Stdout)
-	if err != nil {
-		panic(err)
-	}
 }
