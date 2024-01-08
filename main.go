d3155b3b649a3f1bf8096cffecf4350621d30d57
Abiria <abiria.dev@gmail.com>
2024-01-08 13:59:11 +0900

    feat: implement stdin source

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"mime"
 	"os"
 	"path/filepath"
 
@@ -20,30 +19,43 @@ func main() {
 			},
 		},
 		Action: func(ctx *cli.Context) error {
-			m := ctx.String("mime")
+			m := dataurl.MimeCustom(ctx.String("mime"))
 
 			file := ctx.Args().Get(0)
+			var data *os.File
 
 			if file == "" {
-				panic("provide at least one file")
+				data = os.Stdin
+
+				var buf [128]byte
+				_, err := data.Read(buf[:])
+				if err != nil {
+					panic(err)
+				}
+
+				data.Seek(0, 0)
+
+				m = dataurl.MimeFromBuf(buf[:])
+			} else {
+				d, err := os.Open(file)
+				if err != nil {
+					panic(err)
+				}
+				data = d
+
+				if m == "" {
+					// m = mime.TypeByExtension(filepath.Ext(file))
+					m, err = dataurl.MimeFromExt(filepath.Ext(file))
+					if err != nil {
+						panic(err)
+					}
+				}
+				if m == "" {
+					panic("unknown file type")
+				}
 			}
 
-			h, err := os.Open(file)
-
-			if err != nil {
-				panic(err)
-			}
-
-			if m == "" {
-				m = mime.TypeByExtension(filepath.Ext(file))
-			}
-
-			if m == "" {
-				panic("unknown file type")
-			}
-
-			err = dataurl.ToDataUrl(dataurl.Mime(m), h, os.Stdout)
-
+			err := dataurl.ToDataUrl(dataurl.Mime(m), data, os.Stdout)
 			if err != nil {
 				panic(err)
 			}
